3c665e59ecbfcbcde7ddad460e2293c230bcb67e
Isaac Ongera <isaacongera717@gmail.com>
2023-03-24 07:48:27 +0300

    completed assignment

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,7 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
+  
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -12,6 +14,10 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
